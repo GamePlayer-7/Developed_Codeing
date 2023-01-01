da0955c1e75a72164cc5707d724123942e6a5458
Sagar_Ghadi <69140199+GamePlayer-7@users.noreply.github.com>
2023-01-01 22:28:58 +0530

    Create 03_If_else.py

---
+++ Days_Count_Down/03_If_else.py
@@ -0,0 +1,10 @@
+print("Welcome to Azure Fundamentals Scorecard.")
+
+score = int(input("How much marks you have scored in test? : ")
+            
+if score>=750 :
+    print("Congratulations!!! You have cleared Azure Fundamentals.")
+else :
+    print("Sorry, You need to give the test again.")
+            
+            
